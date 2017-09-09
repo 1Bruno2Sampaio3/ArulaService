a604ec9ad67bcc7ebff9fc0cbfe358d25021f616
lucas <lucas.savioli.scaravelli@gmail.com>
2017-09-08 22:22:53 -0300

    O sistema funciona com banco de dados agora, mudanças no scripts e codigos basicos para rodar o sistema

--- go/src/app.go
+++ go/src/app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -20,26 +22,58 @@ func (a *App) Init(user, password, dbname string) {
 	if err != nil {
 		panic(err)
 	}
-	//only a test
 	//defer s.Close()
 
 	s.SetMode(mgo.Monotonic, true)
 
-	a.Session = s
+	a.Session = s.Copy()
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
 
 }
 
 func (a *App) Run(addr string) {
-	defer a.Session.Close()
+	fmt.Println("listen")
 	log.Fatal(http.ListenAndServe(":7777", a.Router))
 }
 
 func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/users", a.postUsers).Methods("POST")
 	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
+}
+
+func (a *App) postUsers(w http.ResponseWriter, r *http.Request) {
+	var u User
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&u); err != nil {
+		fmt.Println(err)
+		respondWithError(w, http.StatusBadRequest, "Invalid request new user")
+		return
+	}
+
+	if err := u.createUser(a.Session); err != nil {
+		fmt.Println(err)
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, u)
+
 }
 
 func (a *App) getUsers(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
